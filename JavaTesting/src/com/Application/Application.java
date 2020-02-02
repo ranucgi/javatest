8d9adbc945e2c11088ed7dd2856280c1421c9c01
ranucgi <58414024+ranucgi@users.noreply.github.com>
2020-02-02 12:12:23 +0530

    Update Application.java

--- JavaTesting/src/com/Application/Application.java
+++ JavaTesting/src/com/Application/Application.java
@@ -2,7 +2,7 @@ package com.Application;
 
 public class Application {
 	public static void main(String[] args) {
-		System.out.println("welcomeh...........................");
+		System.out.println("welcome back");
 	}
 	
 }
