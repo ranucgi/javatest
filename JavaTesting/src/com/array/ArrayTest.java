563aaec6a9a801005ebaf0157849a145be15faa8
ranu patel <ranu.patel@cgi.com>
2020-04-08 12:22:39 +0530

    confict array

--- JavaTesting/src/com/array/ArrayTest.java
+++ JavaTesting/src/com/array/ArrayTest.java
@@ -4,11 +4,6 @@ public class ArrayTest {
    public static void main(String[] args) {
 
 	System.out.println("raa");
-
-	System.out.println("DSfgdsfdsfs");
-<<<<<<< HEAD
-
-=======
->>>>>>> 97e494ae99fb3f848ee3a5e066ecc611f240e116
-}
+    System.out.println("DSfgdsfdsfs");
+   }
 }
