5ad11499879904460fba97feb7aa4381e576fde2
ranu patel <ranu.patel@cgi.com>
2020-04-08 13:57:10 +0530

    add array

--- JavaTesting/src/com/array/ArrayTest.java
+++ JavaTesting/src/com/array/ArrayTest.java
@@ -3,5 +3,6 @@ package com.array;
 public class ArrayTest {
    public static void main(String[] args) {
    System.out.println("welcome test branch");
+   System.out.println("again data");
    }
 }
